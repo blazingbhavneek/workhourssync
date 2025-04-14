f312f95e9f412b00347592b54d7ae58142533bef
Bhavneek Singh <bhavneek2002@gmail.com>
2025-04-14 16:17:48 +0900

    qr generator 50% done

---
+++ src/app/qrGenerator/page.jsx
@@ -0,0 +1,46 @@
+'use client'
+import { useState, useEffect } from 'react'
+import QRCode from 'react-qr-code'
+
+export default function DynamicQRCode() {
+  const [value, setValue] = useState('');
+  const [isGeneratorOn, setIsGeneratorOn] = useState(false);
+
+  useEffect(() => {
+    const generate = () => {
+      const payload = JSON.stringify({
+        employeeId: 1234567890, // session.user.employeeId,
+        expiry: Date.now() + 10000
+      })
+      let qrCode = window.btoa(payload);
+      console.log(qrCode);
+      setValue(qrCode);
+    }
+    generate()
+    const id = setInterval(generate, 2000)
+    return () => clearInterval(id)
+  }, [])
+
+  return <div className='bg-white flex flex-col justify-center items-center h-screen w-screen'>
+            <div className='absolute top-0 left-0 text-center text-white bg-[#b20303] text-5xl flex flex-row justify-center items-center w-full h-auto p-3.5'>
+                Generate QR Code
+            </div>
+
+            {!isGeneratorOn ? (
+                    <button 
+                    className='w-[100px] p-3 bg-[#0057A6] rounded-2xl'
+                    onClick={() => setIsGeneratorOn(true)}>
+                    Start
+                </button>
+            ) : 
+                <div className='flex flex-col justify-around items-center gap-2.5'>
+                    <QRCode value={value} />
+                    <button 
+                    className='w-[100px] p-3 bg-[#0057A6]'
+                    onClick={() => setIsGeneratorOn(false)}>
+                        Stop
+                    </button>
+                </div>
+            }
+        </div>;
+}
